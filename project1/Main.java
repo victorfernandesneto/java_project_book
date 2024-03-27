1b07f67eacd945146405c4340e66de7da3b302d6
Victor Fernandes Neto <vvfernandesneto@gmail.com>
2024-03-27 12:41:06 -0300

    commit inicial

---
+++ project1/Main.java
@@ -0,0 +1,8 @@
+public class Main { public static void main(String[] args)
+    {String greeting = "Hello, guest!\nWelcome to Mesozoic Eden.";
+        String openingHours = "08:00"; String closingHours = "20:00";
+    String safetyMessage = "Mesozoic Eden is safe and secure.";
+        System.out.println(greeting);
+    System.out.println("We open at " + openingHours + " and close at " + closingHours + ".");
+    System.out.println(safetyMessage);
+}}
